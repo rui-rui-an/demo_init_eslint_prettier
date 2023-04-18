c0054618433eee621ce86ffa5485f57c24f18b25
rui-rui-an <1405613785@qq.com>
2023-04-19 01:17:17 +0800

    测试prettier

--- src/views/Search.vue
+++ src/views/Search.vue
@@ -2,5 +2,23 @@
   <div class="about">
     <h1>This is 就哈哈伤害到收到货 about page</h1>
     <div sajkdhwqe="wqekljsadklsad" sajkdhaswqe="wqekljsadwqeklsad" sajkdhwsqe="wqekljsadkqswelsad" sajkdahwsdqe="wqeksljsadklsad" sajkdasdhwqe="wqekljsadklsad"></div>
+    <div class="father">
+      <div class="child1"></div>
+      <div class="child2"></div>
+      <div class="child3"></div>
+    </div>
   </div>
 </template>
+<script>
+export default {
+  methods: {
+    saaa() {
+      const b = '1'
+      console.log(b)
+    },
+    aaa() {
+      console.log(54545)
+    }
+  }
+}
+</script>
